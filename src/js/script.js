cc8cb11af7ea52af8dd841bffcf8bef0ba5eefdf
Arterius <zaghidullinr@bk.ru>
2021-01-01 15:44:45 +0500

    Создание блока с картой

--- src/js/script.js
+++ src/js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     });
 
     function toggleSlide(item) {
-        $('item').each(function (i) {
+        $(item).each(function (i) {
             $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
