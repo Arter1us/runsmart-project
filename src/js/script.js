75337010db3194ad2a2ab5f650a8960956103dcc
Arterius <zaghidullinr@bk.ru>
2021-01-03 16:12:36 +0500

    Исправление ошибок и добавление анимаций

--- src/js/script.js
+++ src/js/script.js
@@ -98,4 +98,15 @@ $(document).ready(function () {
         return false;
     });
 
+    //Smooth scroll pageup
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    new WOW().init();
+
 });
